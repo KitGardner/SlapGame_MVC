160280c82737f069f09a63b8cdaa50231d550428
Kit Gardner <kit_gardner@ultimatesoftware.com>
2019-10-11 15:30:22 -0600

    Framework done.

--- app/Controllers/GameController.js
+++ app/Controllers/GameController.js
@@ -6,15 +6,51 @@ let _gameService = new GameService();
 export default class GameController {
   constructor() {
     console.log("Hello from the controller");
-
+    this.Update();
   }
 
   Slap() {
     _gameService.DamageEnemy(1);
     this.Update();
   }
-  Update() {
-    console.log("Update the UI");
 
+  SwordSlash() {
+    _gameService.DamageEnemy(5);
+    this.Update();
+  }
+
+  FireBall() {
+    _gameService.DamageEnemy(10);
+    _gameService.AttachItemToEnemy("Fire");
+    this.Update();
+  }
+
+  GiveFire() {
+    _gameService.AttachItemToEnemy("Fire");
+    this.Update();
+  }
+
+  GiveArmor() {
+    _gameService.AttachItemToEnemy("Armor");
+    this.Update();
+  }
+
+  GiveBomb() {
+    _gameService.AttachItemToEnemy("Bomb");
+    this.Update();
+  }
+
+  Update() {
+    let currentEnemyStats = _gameService.enemy;
+    document.getElementById("enemyName").innerText = currentEnemyStats.name;
+    document.getElementById("healthBar").style.width = `${this.CalcHealthBarPercent(currentEnemyStats.maxHealth, currentEnemyStats.health)}%`;
+    document.getElementById("enemyHealth").textContent = currentEnemyStats.health.toString();
+    document.getElementById("attackCount").innerText = currentEnemyStats.hitCount.toString();
+    document.getElementById("modifiers").innerHTML = currentEnemyStats.modifiers;
+  }
+
+  CalcHealthBarPercent(maxValue, currentValue) {
+    const healthBarScale = 0.8;
+    return ((currentValue / maxValue) * healthBarScale) * 100;
   }
 }
--- app/Services/GameService.js
+++ app/Services/GameService.js
@@ -10,11 +10,72 @@ let _items = {
 
 export default class GameService {
   DamageEnemy(damage) {
-    _enemy.health -= damage;
+    _enemy.health -= damage + this.AddMods();
     _enemy.hitCount++;
     console.log(_enemy.health);
 
   }
+
+  FormatModifiers() {
+    let modifierTemplate = "";
+    for (let i = 0; i < _enemy.mods.length; i++) {
+      const mod = _enemy.mods[i];
+      modifierTemplate += `
+        <div>
+          <img src=${mod.imgUrl} class="modifierImg">
+          <span>${mod.modifier}</span>
+        </div>
+      `
+    }
+
+    debugger
+
+    return modifierTemplate;
+  }
+
+  AddMods() {
+    let totalModification = 0;
+    let modsToDelete = [];
+
+    for (let i = 0; i < _enemy.mods.length; i++) {
+      const mod = _enemy.mods[i];
+      totalModification += mod.modifier;
+      mod.lifeSpan--;
+
+      if (mod.lifeSpan == 0) {
+        modsToDelete.push({
+          modName: mod.name,
+          modIndex: i
+        })
+      }
+    }
+
+    this.RemoveModifiers(modsToDelete);
+    return totalModification;
+  }
+
+  RemoveModifiers(modsToDelete) {
+    modsToDelete.reverse().forEach(mod => {
+      _enemy.mods.splice(mod.modIndex, 1);
+    })
+  }
+
+  AttachItemToEnemy(itemName) {
+    _enemy.mods.push(_items[itemName]);
+  }
+
+  get enemy() {
+    let enemyStats = {
+      name: _enemy.name,
+      health: _enemy.health,
+      maxHealth: _enemy.maxHealth,
+      hitCount: _enemy.hitCount,
+      modifiers: this.FormatModifiers()
+    };
+
+    return enemyStats;
+  }
+
   constructor() {
     console.log("Hello from the game service");
 
